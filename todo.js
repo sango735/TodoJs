3273e5318b07fca1f3667599e88e26d4774a0f69
sango735 <735th.floor@gmail.com>
2021-03-11 18:27:43 +0900

    追加した数だけ表示する機能実装

--- todo.js
+++ todo.js
@@ -1,13 +1,15 @@
-let todo = () =>{
+function todo() {
     const form = document.getElementById("todo-form");
     const output = document.getElementById("output-todo");
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        let inputTodo = document.getElementById("input-todo");
-        let inputValue =  inputTodo.value;
-        let checkBox = '<div class="form-check"><input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">';
-        let listTodo = checkBox + '<label class="form-check-label" for="flexCheckChecked">' + inputValue + '</label></div>';
-        output.innerHTML = listTodo;
+        const inputTodo = document.getElementById("input-todo");
+        const inputValue =  inputTodo.value;
+        const checkBox = '<div class="form-check"><input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">';
+        const listTodo = checkBox + '<label class="form-check-label" for="flexCheckChecked">' + inputValue + '</label></div>';
+        output.insertAdjacentHTML("afterend", listTodo);
     });
 }
+
+window.addEventListener("load", todo);
