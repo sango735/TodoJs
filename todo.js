12811603d39ba14a6779ef65c9f110c92bf4f4e1
sango735 <735th.floor@gmail.com>
2021-03-12 11:50:32 +0900

    リファクタリング実施

--- todo.js
+++ todo.js
@@ -2,20 +2,20 @@ function todo() {
     const form = document.getElementById("todo-form");
     const output = document.getElementById("output-todo");
     const submit = document.getElementById("todo-submit");
+    const inputTodo = document.getElementById("input-todo");
 
+    //入力した内容が空白の場合にボタンを押せなくする処理
     form.addEventListener("input", () =>{
-        const inputTodo = document.getElementById("input-todo");
         const inputValue =  inputTodo.value;
         if(inputValue != ""){
             submit.disabled = false;
-        }else{
-            submit.disabled = true;
         }
     });
+
+    //入力内容の送信とフォームのリセット処理
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        const submitTodo = document.getElementById("input-todo");
-        const submitValue =  submitTodo.value;
+        const submitValue =  inputTodo.value;
         const checkBox = '<div class="form-check"><input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">';
         const listTodo = checkBox + '<label class="form-check-label" for="flexCheckChecked">' + submitValue + '</label></div>';
         output.insertAdjacentHTML("beforeend", listTodo);
